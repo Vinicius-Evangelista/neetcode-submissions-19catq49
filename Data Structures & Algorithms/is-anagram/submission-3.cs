5649eafb6411c446a91f8235ed09e767422f7850
Vinicius Evangelista <82423270+Vinicius-Evangelista@users.noreply.github.com>
2026-04-09 13:23:29 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,16 @@
+public class Solution {
+ public bool IsAnagram(string s, string t) {
+    if (s.Length != t.Length) return false;
+
+    int[] count = new int[26];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+
+    return count.All(x => x == 0);
+}
+    
+}
