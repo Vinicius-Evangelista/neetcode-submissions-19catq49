c63701f66298031017ad18cbfad0e8c32116bb06
Vinicius Evangelista <82423270+Vinicius-Evangelista@users.noreply.github.com>
2026-04-15 18:10:56 -0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+
+        const string delim = "#";
+        var sb = new StringBuilder(200);
+
+        foreach(var s in strs)
+        {
+            var length = s.Length;
+            sb.Append(length).Append(delim).Append(s);  
+        }
+
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s) {
+
+        List<string> strs = new ();
+
+        int start = 0;
+        var sSpan = s.AsSpan();
+
+        while(start < s.Length)
+        {
+            int hashIndex = s.IndexOf('#', start);
+            int count = int.Parse(s[start..hashIndex]);
+            var end = hashIndex + 1 + count;
+            strs.Add(s[(hashIndex + 1)..(end)]);
+            start = end;
+        }
+
+        return strs;
+   }
+}
