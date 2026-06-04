70b2a932a50bcffa6c0b183d5ee7d136effd83ec
Vinicius Evangelista <82423270+Vinicius-Evangelista@users.noreply.github.com>
2026-06-04 10:56:50 -0300

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cs
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        if (lists == null || lists.Length == 0)
+            return null;
+
+        while (lists.Length > 1) {
+            List<ListNode> mergedLists = new();
+
+            for (int i = 0; i < lists.Length; i += 2) {
+
+                ListNode l1 = lists[i];
+                ListNode l2 = (i + 1) < lists.Length ? lists[i + 1] : null;
+                mergedLists.Add(MergeList(l1, l2));
+            }
+
+            lists = mergedLists.ToArray();
+        }
+        
+        return lists[0];
+    }
+
+    public ListNode MergeList(ListNode l1, ListNode l2) {
+        ListNode dummy = new();
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            
+            tail = tail.next;
+        }
+
+        if (l1 != null)
+            tail.next = l1;
+        if (l2 != null)
+            tail.next = l2;
+
+        return dummy.next;
+    }
+}
